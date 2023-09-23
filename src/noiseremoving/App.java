47410f5d58851a7336b4212d3054f840b83651ee
wsking233 <wsking233@gmail.com>
2023-09-24 04:07:29 +1300

    jar Pack has been created.

--- src/noiseremoving/App.java
+++ src/noiseremoving/App.java
@@ -16,15 +16,16 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 import java.awt.event.ActionEvent;
+
 /**
  *
  * @author William Wang
  * @StudnetID 18017970
- * 
- * This class is used to create the GUI for the noise removing program.
- * Will be new in the main method of the NoiseRemoving class to show the GUI.
  *
- * 
+ * This class is used to create the GUI for the noise removing program. Will be
+ * new in the main method of the NoiseRemoving class to show the GUI.
+ *
+ *
  */
 public class App {
 
@@ -57,82 +58,71 @@ public class App {
         this.image = null;
         this.imageProcess = null;
         this.fileChooser = new JFileChooser(System.getProperty("user.dir")); // set the default directory to the current
-                                                                             // directory
+        // directory
 
         // Open image button:
         this.openButton = new JButton("Open Image");
-        this.openButton.addActionListener(new ActionListener() {
-            // add action listener to the buttons
-            // this button is used to open file chooser to select image
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                int stateImageFileChooser = fileChooser.showOpenDialog(frame);
-                if (stateImageFileChooser == JFileChooser.APPROVE_OPTION) {
+        this.openButton.addActionListener((ActionEvent e) -> {
+            int stateImageFileChooser = fileChooser.showOpenDialog(frame);
+            if (stateImageFileChooser == JFileChooser.APPROVE_OPTION) {
 
-                    selectedFile = fileChooser.getSelectedFile();
-                    try {
-                        image = ImageIO.read(selectedFile);
-                    } catch (IOException exception) {
-                        exception.printStackTrace();
-                    }
-                    if (image != null) { // check if image is not null before setting the icon
-                        imageLabel.setIcon(new ImageIcon(image));
-                        frame.setSize(image.getWidth() + 10, image.getHeight() + 100);
-                    }
+                selectedFile = fileChooser.getSelectedFile();
+                try {
+                    image = ImageIO.read(selectedFile);
+                } catch (IOException exception) {
+                    JOptionPane.showMessageDialog(frame, exception.getMessage());
                 }
-            }
-
-        });
-
-
-        // Process image button:
-        this.processButton = new JButton("Remove Noise");
-        this.processButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                // this button is used to process the image
-                // if the image is not opened, then show error message
-                // if the image is opened, then process the image
-                if (image == null) {
-                    JOptionPane.showMessageDialog(null, "Please open an image first!");
-                } else {
-                    imageProcess = new ImageProcess(selectedFile.getPath());
-                    imageProcess.cleanNoise();
-                    image = imageProcess.buffered_image;
+                if (image != null) { // check if image is not null before setting the icon
                     imageLabel.setIcon(new ImageIcon(image));
                     frame.setSize(image.getWidth() + 10, image.getHeight() + 100);
                 }
             }
-        });
+        } // add action listener to the buttons
+        // this button is used to open file chooser to select image
+        );
 
+        // Process image button:
+        this.processButton = new JButton("Remove Noise");
+        this.processButton.addActionListener((ActionEvent e) -> {
+            // this button is used to process the image
+            // if the image is not opened, then show error message
+            // if the image is opened, then process the image
+            if (image == null) {
+                JOptionPane.showMessageDialog(null, "Please open an image first!");
+            } else {
+                imageProcess = new ImageProcess(selectedFile.getPath());
+                imageProcess.cleanNoise();
+                image = imageProcess.buffered_image;
+                imageLabel.setIcon(new ImageIcon(image));
+                frame.setSize(image.getWidth() + 10, image.getHeight() + 100);
+            }
+        });
 
         // Save image button:
         this.saveButton = new JButton("Save Image");
-        this.saveButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                // this button is used to save the image
-                // if the image is not processed, then save the original image
-                // if the image is processed, then save the processed image
-                if (imageLabel.getIcon() == null) {
-                    JOptionPane.showMessageDialog(null, "Please open an image first!");
-                } else {
-                    // imageProcess.save("noise_removed.jpg");
-                    // imageLabel.setIcon(null);
-                    int stateImageFileChooser = fileChooser.showSaveDialog(frame);
-                    if (stateImageFileChooser == JFileChooser.APPROVE_OPTION) {
-                        File saveFile = fileChooser.getSelectedFile();
-                        try {
-                            ImageIO.write(image, "jpg", saveFile);
-                            JOptionPane.showMessageDialog(null, "Image saved!");
-                            imageLabel.setIcon(null);   // clear the image label after the image is saved.
-                        } catch (IOException exception) {
-                            // exception.printStackTrace();
-                            JOptionPane.showMessageDialog(frame, exception.getMessage());
-                        }
-                    }else{
-                        JOptionPane.showMessageDialog(null, "No image to save!");
+        this.saveButton.addActionListener((ActionEvent e) -> {
+            // this button is used to save the image
+            // if the image is not processed, then save the original image
+            // if the image is processed, then save the processed image
+            if (imageLabel.getIcon() == null) {
+                JOptionPane.showMessageDialog(null, "Please open an image first!");
+            } else {
+                // imageProcess.save("noise_removed.jpg");
+                // imageLabel.setIcon(null);
+                int stateImageFileChooser = fileChooser.showSaveDialog(frame);
+                if (stateImageFileChooser == JFileChooser.APPROVE_OPTION) {
+                    File saveFile = fileChooser.getSelectedFile();
+                    try {
+                        ImageIO.write(image, "jpg", saveFile);
+                        JOptionPane.showMessageDialog(null, "Image saved!");
+                        image = null;  //clear the image a
+                        imageLabel.setIcon(null);   // clear the image label after the image is saved.
+                    } catch (IOException exception) {
+                        // exception.printStackTrace();
+                        JOptionPane.showMessageDialog(frame, exception.getMessage());
                     }
+                } else {
+                    JOptionPane.showMessageDialog(null, "No image to save!");
                 }
             }
         });
